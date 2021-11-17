ab3f9f0b297edd89ff9eaa54567e5d0d78644e8e
givemefivw <2143889877@qq.com>
2021-11-17 11:18:40 +0800

    XradScan
    XradScan即是将xray和rad联动，启动xray监听端口，利用http-proxy将rad扫描带入xray，实现联动

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
+XradScan自述文档
+
+------
+
 # XradScan
+
+XradScan即是将xray和rad联动，启动xray监听端口，利用http-proxy将rad扫描带入xray，实现联动。
+
+![img](/image.png)
+
+# Who Use XradScan
+
+那必须是渗透测试人员啦。
+
+其实主要是为了方便批量进行漏扫，之前写过一个类似的简单脚本AutoXray`https://github.com/givemefivw/AutoXray`，同样是为了方便批量扫描。
+
+# How It Works
+
+其实我觉得这一项不必要写。
+
+观察源码就可以看到，就是使用了os.system(我都没用subprocess)调用xray和rad工具执行命令。
+
+啥？技术点？
+
+那没有2333
+
+# Usage
+
+```JavaScript
+____  ___                  .___
+\   \/  /___________     __| _/
+ \     /\_  __ \__  \   / __ |
+ /     \ |  | \// __ \_/ /_/ |
+/___/\  \|__|  (____  /\____ |
+      \_/           \/      \/
+
+                @Givemefivw
+
+usage: XradScan.py [-h] [-f FILE]
+
+Xrad Scan Help
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FILE, --file FILE  Please set the target urlfile
+```
+
+很简单，只有一个参数，将目标网址写进txt文件里就可以开始扫描了。
+
+# Installation
+
+```JavaScript
+git clone 
+cd XradScan
+python3 XradScan.py -f url.txt
+```
+
+# Config
+
+只需要将xray和rad放在同一目录下即可，注意两者的配置文件，有的网站默认不允许爬取，视情况修改配置文件。
---
+++ XradScan.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# Author: @Givemefivw
+# Descrip: Xray&rad联动扫描
+import time
+import os
+import re
+import argparse
+import sys
+from typing import Pattern
+
+def banner():
+	banner = '''
+____  ___                  .___
+\   \/  /___________     __| _/
+ \     /\_  __ \__  \   / __ | 
+ /     \ |  | \// __ \_/ /_/ | 
+/___/\  \|__|  (____  /\____ | 
+      \_/           \/      \/ 
+	
+		@Givemefivw
+	'''
+	print(banner)
+
+
+def xrad(file):
+	try:
+		outime = '%date:~0,4%%date:~5,2%%date:~8,2%0%time:~1,1%%time:~3,2%%time:~6,2%'
+		run_xray = os.system('start cmd /k xray.exe webscan --listen 127.0.0.1:7799 --html-output Xrad-{}.html'.format(outime))
+		time.sleep(5)
+		target = open(file,'r')
+		lines = target.readlines()
+		pattern = re.compile(r'^(https|http)://')
+		for line in lines:
+			if not pattern.match(line.strip()):
+				url = 'http://' + line.strip()
+			else:
+				url = line.strip()
+		run_rad = os.system('start cmd /k rad.exe -t {} -http-proxy 127.0.0.1:7799'.format(url))
+		target.close()
+	except Exception as e:
+		pass
+	except KeyboardInterrupt:
+		pass
+
+def main():
+	parser = argparse.ArgumentParser(description='Xrad Scan Help')
+	parser.add_argument('-f','--file',help=' Please set the target urlfile',default='')
+	args = parser.parse_args()
+
+	if args.file:
+		file = args.file
+		xrad(file)
+if __name__ == '__main__':
+	banner()
+	main()
